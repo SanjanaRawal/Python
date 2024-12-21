0cecbcdce498d69e6c51e5ee3764f8c5fc606733
SanjanaRawal <sanjana.rawal2006@gmail.com>
2024-12-21 19:48:50 +0530

    More patterns

---
+++ 03_Loops/Patterns/13.py
@@ -0,0 +1,8 @@
+for i in range(1 , 7 , 1) :
+    for j in range(1 , i+1 , 1) :
+        print(" * " , end = " ")
+    print()
+for i in range(7 , 0 , -1) :
+    for j in range(1 , i-1 , 1) :
+        print(" * " , end=" ")
+    print()
