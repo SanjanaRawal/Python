ca70852dfd72fb7ea4318ebbdd4fc2ea700308cb
SanjanaRawal <sanjana.rawal2006@gmail.com>
2024-12-24 19:50:11 +0530

    Dictionaries

---
+++ 04_DataStructures/06_Dictionary.py
@@ -0,0 +1,10 @@
+dict1 = {"Name" : "ABC " , "Age" : 23 , "RollNo" : 45 , "Weight" : 56.9 , "Pass" : True}
+print(dict1)
+print(type(dict1))
+#Slicing in dictionaries
+print(dict1["Age"])
+dict1["Name"]= "XYZ"
+print(dict1)
+print(dict1["Name"])
+print(dict1["Pass"])
+print(type(dict1["Pass"]))
---
+++ 04_DataStructures/07_DictionaryFunctions.py
@@ -0,0 +1,12 @@
+dict1 = {"Name" : "ABC " , "Age" : 23 , "RollNo" : 45 , "Weight" : 56.9 , "Pass" : True}
+print(dict1.get("Name"))
+print(dict1.items())
+print(dict1.values())
+print(dict1.keys())
+dict1.clear()
+print(dict1)
+dict1.setdefault(2)
+print(dict1)
+d = {"H" : "Hello" , "W" : "World"}
+dict1 = d.copy()
+print(dict1)
