6c67942134237dcdfecb32309975ef0f1062b418
SanjanaRawal <sanjana.rawal2006@gmail.com>
2024-12-24 16:23:15 +0530

    Lists

---
+++ 01_Basics/16_Lists.py
@@ -0,0 +1,3 @@
+lst = [123 , "abc" , 34.5 , True , 876 , "xyz" , False , 0]
+print(lst)
+print(type(lst))
---
+++ 01_Basics/17_ListFunctions.py
@@ -0,0 +1,35 @@
+from os import remove
+
+lst = [123 , "abc" , 34.5 , True , 876 , "xyz" , False , 0 , True]
+#To get size of list
+print("List size = " , len(lst))
+#To count occurrence of an element
+print("Occurrence of True : " , lst.count(True))
+#Add something to list
+lst.append([1,2,3])
+print("New list : \n" , lst)
+#Add at specific location
+lst.insert(2 , (99,55))
+print("New list : \n" , lst)
+#To remove from list
+lst.remove(True)
+print("New list : \n" , lst)
+#To remove from certain location
+lst.pop(2)
+print("New list : \n" , lst)
+#To create copy of list
+lst2 = lst.copy()
+print("Other list : " , lst2)
+#To access elements
+print("Index of abc is " , lst.index("abc") )
+#To extend one list to other
+lst3 = [3 , 4 , 5]
+lst.extend(lst3)
+print("Extended list : " , lst)
+#To reverse list
+print("Reversed list : " , lst.reverse())
+#To sort list
+a = [5,8,7 , 0 , -8]
+print("Unsorted list : " , a)
+a.sort()
+print("Sorted list : " ,a)
