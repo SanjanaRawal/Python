7190fb24200d5ad66881db74069a254cb4f70247
SanjanaRawal <sanjana.rawal2006@gmail.com>
2024-12-24 15:54:51 +0530

    Strings

---
+++ 01_Basics/14_StringFunctions.py
@@ -0,0 +1,62 @@
+str = "Indore is the cleanest city."
+print(str)
+#to find length of string
+print("Length of string = " , len(str))
+#to count any letter in string
+print("Total Occurrence of letter t = " , str.count("t"))
+#to count any letter in string in a given range
+print("Total Occurrence of letter t in given range = " , str.count("t", 2 , 20))
+#to convert str into uppercase
+print("Upper case : " , str.upper())
+#to convert str into lowercase
+print("Lower case : " , str.lower())
+#to make 1st letter capital
+print("Capitalized : " , str.capitalize())
+#to get index of a letter
+print("1st occurrence of t : " , str.index("t"))
+print("1st occurrence of t : " , str.find("t"))
+#find returns -1 if not found , index returns error
+#to get index of a letter over a range
+print("1st occurrence of t in given range : " , str.index("t" , 11))
+''''#to set a format
+city = input("Enter your city : ")
+result = "I live in {} "
+print(result.format(city))'''
+#to bring a string at between
+print(str.center(100))
+print(str.center(100 , "*"))
+#Check for all characters as alphanumeric
+print("All alphanumeric? " , str.isalnum())
+#Check for all characters as alphabets
+print("All alphabets? " , str.isalpha())
+#Check for all characters as numeric
+print("All numeric? " , str.isnumeric())
+#Check for all characters as decimals
+print("All decimals? " , str.isdecimal())
+#Check for all characters as digits
+print("All digits? " , str.isdigit())
+#Check for all lower case
+print("All lower cased? " , str.islower())
+# Check for all upper case
+print("All upper cased? " , str.isupper())
+#Check for all spaces
+print("All spaces? " , str.isspace())
+#Check for starting letter as capital and rest as lower case
+print("Title cased? " , str.istitle())
+#To check how string ends
+print("Ends with '.'? " , str.endswith("."))
+#To check how string starts
+print("Starts with 'I' ? " , str.startswith("I"))
+#Lower to upper and vice versa
+print("Swap cased : " , str.swapcase())
+#To split the string
+print("String after split : " , str.split(" "))
+#Left justified string
+print("Left justified as mentioned : \n" , str.ljust(10,))
+#Right justified string
+print("Right justified as mentioned : \n" , str.rjust(10))
+#To replace a character/s
+print("Replace the with d : " , str.replace("the" , "d"))
+#To get last index of a letter
+print("Last index of t is " , str.rindex("t"))
+
---
+++ 01_Basics/15_SlicingInStrings.py
@@ -0,0 +1,13 @@
+str = "I'm working on pycharm"
+print(str[:])
+print(str[::])
+print(str[0:len(str):1])
+print(str[0:len(str)-1:1])
+print(str[2:7])
+print(str[2:9:1])
+print(str[::2])
+print(str[5:8:2])
+print(str[::5])
+print(str[len(str) : 0 : -2])
+#To reverse the string
+print(str[::-1])
