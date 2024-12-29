fd5b9752499023b712df25370f878cf0f0ab940b
SanjanaRawal <sanjana.rawal2006@gmail.com>
2024-12-29 22:07:22 +0530

    numpy

--- 06_Modules&Libraries/05_Numpy.py
+++ 06_Modules&Libraries/05_Numpy.py
@@ -1,5 +1,5 @@
 import numpy as np 
-
+import statistics as stats
 arr = np.array([1,2,3,4,5,6])
 print(arr)
 print(type(arr))
@@ -79,6 +79,89 @@ print("Combining and splitting array".center(50,"*"))
 a3 = np.array([1,2 , 3,4])
 a4 = np.array([99 , 876])
 print(np.concatenate([a3,a4]))
-print(np.concatenate([a1,a2]))
-print(np.concatenate([a1,a2],axis=0))
-print(np.concatenate([a1,a2] , axis=1))
+print(np.concatenate([a1,a2])) #by default vertical
+print(np.concatenate([a1,a2],axis=0)) #concatenate vertically
+print(np.vstack([a1,a2])) #concatenate vertically
+print(np.concatenate([a1,a2] , axis=1)) #concatenate horizontally
+print(np.hstack([a1,a2])) #concatenate horizontally
+
+a = np.array([20,30,40,40,30,20])
+print(np.array_split(a,2))
+print(np.array_split(a,3))
+print(np.array_split(a,4))
+print(np.array_split(a,4)[2])
+print(np.array_split(a,3)[0])
+print(np.array_split(a,2)[-1])
+b = np.array([[1,2,3],[2,5,7]])
+print(np.array_split(b,2))
+print(np.array_split(b,3))
+print(np.array_split(b,4))
+print(np.array_split(b,4)[2])
+print(np.array_split(b,3)[0])
+print(np.array_split(b  ,2)[-1])
+
+print()
+print("Adding and removing elements")
+arr = np.array([1,2,3,4,5])
+print(np.append(arr,23))
+print(np.append(arr,[78,99]))
+print(np.insert(arr,2,98))
+print(np.insert(arr,[2,5],[77,65]))
+print(arr)
+print(np.delete(arr,2)) #Element deleted at 2nd index i.e. 3
+arr2 = np.array([[1,2,3],[4,5,6]])
+print(np.append(arr2 , 55)) #2d converted to 1d by its own
+print(np.append(arr2 , [88 , 73 , 10]))
+print(np.insert(arr2,1,45))
+print(np.insert(arr2,[1],[45,92]))
+print(np.insert(arr2,1,[66,99],axis=1)) #inserted in 2d array
+print(np.insert(arr2,1,[66],axis=1)) 
+print(arr2)
+print(np.delete(arr2,2)) #2d to 1d , delete index 2
+print(np.delete(arr2,2 , axis=1)) #3rd column with index 2 deleted 
+print(np.delete(arr2,0,axis=0)) #1st row with index 0 is deleted 
+
+print()
+print("Sort , filter ans search techniques".center(50,"*"))
+a = np.array([45,23,12,89,67])
+print(np.sort(a))
+print(np.where(a == 12))
+print(np.where(a%3 == 0))
+a2 = np.array([[5,1,3],[6,2,4]])
+print(np.sort(a2))
+print(np.where(a2==6))
+sortedarr = np.array([11,23,45,67,89,90])
+print(np.searchsorted(sortedarr,45))
+arr=np.array([1,2,3,4,5,6])
+print(arr[arr<4])
+print(arr[arr%2==0])
+filtr = [True,False,True,False,True,False]
+print(arr[filtr])
+
+print()
+print("Aggregating functions in arrays".center(50,"*"))
+a = np.array([12,57,82,34,60])
+print(np.sum(a))
+print(np.min(a))
+print(np.max(a))
+print(np.size(a))
+print(np.mean(a))
+print(np.cumsum(a)) #cumulative sum 
+print(np.cumprod(a)) #cumulative product
+price=np.array([10,20,30,40])
+quantity=np.array([5,3,4,2])
+print("Total amount = " , np.cumprod([price , quantity] , axis=0)[1].sum()) #first price*quantity , then their sum
+
+print()
+print("Statistical Functions".center(50,"*"))
+data = np.array([20,50,35,60,10,95,10])
+print(np.mean(data)) #sum of values / it's nu,ber
+print(np.median(data)) #Central value
+print(stats.mode(data)) #No mode in numpy library , so statistics library is imported , gives max occurring value
+print(np.std(data)) #Standard deviation , if less means data is not at distance from mean i.e. data is less spread
+print(np.var(data)) #Variance = square of std , used for quality check 
+price=np.array([10,20,30,40])
+quantity=np.array([5,3,4,2])
+print(np.corrcoef(price,quantity)) 
+#Correlation coefficient value : -1=inversely proportional , +1=proportional , 0=neutral
+#corrcoef used to check dependency between two factors 
