567d953baf224b0ec95b78601564b06be23bc5ce
SanjanaRawal <sanjana.rawal2006@gmail.com>
2025-01-08 19:52:50 +0530

    Advanced Age Calculator

--- 00_Projects/02_AgeCalculator.py
+++ 00_Projects/02_AgeCalculator.py
@@ -1,14 +1,17 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 
-print("Hello , Today we will calculate the age!!!!!!")
-person_name = input("Enter person whose age is to be calculated : ")
-birth_year = int(input("Enter birth year : "))
-birth_month = int(input("Enter birth month in figures : "))
-current_year = datetime.datetime.now().year
-current_month = datetime.datetime.now().month
-person_age = 0
-if birth_month > current_month :
-    person_age = current_year - birth_year - 1
-else :
-    person_age = current_year - birth_year
-print(person_name.capitalize() , " is " , person_age , " years old." )
+print("Hello, Today we will calculate the age!!!!!!")
+person_name = input("Enter the person whose age is to be calculated : ")
+print("Please take care of the format :) ")
+d = input("Enter DOB in format YYYY-MM-DD: ")
+
+DOB = datetime.datetime.strptime(d, "%Y-%m-%d")
+current = datetime.datetime.now()
+
+person_age = relativedelta(current, DOB)
+
+if DOB.month == current.month and DOB.day == current.day:
+    print(f"Happy birthday, {person_name}! Today you're {person_age.years} years old.")
+else:
+    print(f"{person_name.capitalize()} is {person_age.years} years, {person_age.months} months and {person_age.days} days old.")
